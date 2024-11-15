480be8734b827829445225430eae9ac03b449575
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-11-15 22:14:00 +0700

    refactor: Refactor code for better organization and readability

--- repositories.js
+++ repositories.js
@@ -7,28 +7,28 @@ const supabase = createClient(
 const tableChatId = process.env.TABLE_NAME_CHAT_ID;
 
 async function insertDataUser(chatId, username) {
-    return await supabase
-      .from("chat_ids_telegram")
-      .insert({ chat_id: chatId, username: username });
-  }
-  
-  async function getAllDataUsers() {
-    return await supabase
-      .from(tableChatId)
-      .select("chat_id");
-  }
-  
-  async function checkExistingDataUsers(chatId, username) {
-    return await supabase
-      .from(tableChatId)
-      .select("chat_id, username")
-      .eq("chat_id", chatId)
-      .eq("username", username)
-      .single();
-  }
+  return await supabase
+    .from("chat_ids_telegram")
+    .insert({ chat_id: chatId, username: username });
+}
 
-  module.exports = {
-    insertDataUser,
-    getAllDataUsers,
-    checkExistingDataUsers,
-  }
+async function getAllDataUsers() {
+  return await supabase
+    .from(tableChatId)
+    .select("chat_id");
+}
+
+async function checkExistingDataUsers(chatId, username) {
+  return await supabase
+    .from(tableChatId)
+    .select("chat_id, username")
+    .eq("chat_id", chatId)
+    .eq("username", username)
+    .single();
+}
+
+module.exports = {
+  insertDataUser,
+  getAllDataUsers,
+  checkExistingDataUsers,
+};
