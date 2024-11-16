e39e7fc0db7267a925ea92a8ac03a5e757da35ec
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-11-16 22:17:13 +0700

    chore: Improved logging for user insertion process

--- repositories.js
+++ repositories.js
@@ -32,7 +32,6 @@ async function getAllDataUsers(typeMessage) {
 
 // Repository for checking existing user before insert new user
 async function checkExistingDataUsers(chatId, username) { 
-  console.log('Checking existing');
   let query = supabase.from(tableChatId).select('chat_id, username');
 
   if (chatId) query = query.eq('chat_id', chatId);
--- services.js
+++ services.js
@@ -7,11 +7,13 @@ async function onStart(msg) {
   const chatId = msg.chat.id;
   const username = msg.from.username;
   const settings = resources.listFeatures;
+  console.log('Checking existing');
 
   const { data, error: selectError } = await repositories.checkExistingDataUsers(chatId, username);
   if (selectError) throw new Error(`${selectError.message}`);
   if (data) throw new Error(`${username} already exists`);  
   
+  console.log(`Data ${username} not found, inserting...`);
   await repositories.insertDataUser(chatId, username, settings);
 
   console.log(`Data ${username} inserted successfully`);
