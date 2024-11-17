21b2bf710e149f86a04202dce6055053d40fcdba
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-11-17 09:40:20 +0700

    feat: Switched bot to webhook mode

--- app.js
+++ app.js
@@ -15,7 +15,22 @@ app.use(express.json());
 // Setup Bot
 const TelegramBot = require('node-telegram-bot-api');
 const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(telegramBotToken, { polling: true });
+const bot = new TelegramBot(telegramBotToken, { webHook: true });
+
+// Set webhook URL
+const publicUrl = process.env.PUBLIC_URL; //
+const webhookPath = `/webhook/${telegramBotToken}`;
+const webhookUrl = `${publicUrl}${webhookPath}`;
+
+bot.setWebHook(webhookUrl).then(() => {
+    console.log(`Webhook set to: ${webhookUrl}`);
+});
+
+// List action and API routes
+app.post(webhookPath, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
 
 // List action and api routes
 bot.onText(/\/start/, controllers.onStartController);
