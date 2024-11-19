2cff44e19a7dd1c7daa574dc1c4614e73f82844d
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-11-19 13:49:37 +0700

    chore: Refine cron job logic and scheduling

--- api/cron.js
+++ api/cron.js
@@ -1,9 +1,12 @@
 export default function handler(req, res) {
-    // Logika cron job Anda di sini
-    console.log("Cron job executed!");
-  
-    // Contoh response
-    res.status(200).json({ message: "Cron job executed successfully!" });
-  };
+  // Logika cron job Anda di sini
+  console.log('Cron job executed!');
 
-  
+  // Contoh response
+  res.status(200).json({ message: 'Cron job executed successfully!' });
+}
+
+export const config = {
+  runtime: 'edge',
+  schedule: '30 4 * * *'
+};
