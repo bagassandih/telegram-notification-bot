43987ddb79d8be05846a6d820a85e979988b59e3
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-11-19 13:58:46 +0700

    feat: Add daily cron job with greeting

--- api/cron.js
+++ api/cron.js
@@ -1,12 +1,3 @@
 export default function handler(req, res) {
-  // Logika cron job Anda di sini
-  console.log('Cron job executed!');
-
-  // Contoh response
-  res.status(200).json({ message: 'Cron job executed successfully!' });
+  res.status(200).end('Hello Cron!');
 }
-
-export const config = {
-  runtime: 'edge',
-  schedule: '30 4 * * *'
-};
